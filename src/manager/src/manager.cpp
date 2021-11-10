392b4cc282d202e6cbde78dda4e69ba387494c1a
dukun <dukun1@xiaomi.com>
2021-11-10 20:17:47 +0800

    [save] service and topic worked
    Signed-off-by: dukun <dukun1@xiaomi.com>

--- src/manager/src/manager.cpp
+++ src/manager/src/manager.cpp
@@ -1,9 +1,13 @@
 #include <cstdio>
+#include <chrono>
+#include <unistd.h> 
+#include <sys/syscall.h> 
 #include "rclcpp/rclcpp.hpp"
 #include "protocol/msg/tank_active.hpp"
+#include "protocol/srv/probe.hpp"
 
 void sub_tank_running(const protocol::msg::TankActive::SharedPtr msg) {
-  printf("manager get msg: %d\n", msg->activity);
+  printf("manager get msg: %d, tid: %ld, thread id: %ld\n", msg->activity, syscall(SYS_gettid), std::this_thread::get_id());
 }
 
 int main(int argc, char ** argv)
@@ -12,11 +16,32 @@ int main(int argc, char ** argv)
   // (void) argv;
   printf("hello world manager package\n");
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("manager");
+  auto node_ptr = rclcpp::Node::make_shared("manager");
 
-  auto sub = node->create_subscription<protocol::msg::TankActive>("tank_running", rclcpp::SystemDefaultsQoS(), sub_tank_running);
+  auto sub = node_ptr->create_subscription<protocol::msg::TankActive>("tank_running", rclcpp::SystemDefaultsQoS(), sub_tank_running);
 
-  rclcpp::spin(node);
+  auto client_ptr = node_ptr->create_client<protocol::srv::Probe>("tank_calling");
+  auto request_ptr = std::make_shared<protocol::srv::Probe::Request>();
+  while(! client_ptr->wait_for_service(std::chrono::seconds(1))) {
+    if(! rclcpp::ok()) {
+      fprintf(stderr, "manager got error with rcl not ok!\n");
+      return 0;
+    }
+    fprintf(stdout, "manager waiting for service ready...\n");
+  }
+  while(rclcpp::ok()) {
+    fprintf(stdout, "manager client running...\n");
+    auto result = client_ptr->async_send_request(request_ptr);
+    if(rclcpp::spin_until_future_complete(node_ptr, result) == rclcpp::FutureReturnCode::SUCCESS) {
+      fprintf(stdout, "manager get service result: distance: %d, landform: %d. tid: %ld, thread id: %ld\n",
+       result.get()->distance, result.get()->landform, syscall(SYS_gettid), std::this_thread::get_id());
+    } else {
+      fprintf(stderr, "manager call service failed!\n");
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  } 
+  
+  rclcpp::spin(node_ptr);
   rclcpp::shutdown();
   return 0;
 }
--- src/tank/src/tank.cpp
+++ src/tank/src/tank.cpp
@@ -1,25 +1,42 @@
 #include <cstdio>
 #include <chrono>
+#include <thread>
 #include "rclcpp/rclcpp.hpp"
 #include "protocol/msg/tank_active.hpp"
+#include "protocol/srv/probe.hpp"
 
 class Tank : public rclcpp::Node
 {
+using tankmsg = protocol::msg::TankActive;
+using tanksrv = protocol::srv::Probe;
 public:
   Tank() : Node("try") {
     m_pPublisher = this->create_publisher<protocol::msg::TankActive>("tank_running", 10);
+    m_pService = this->create_service<protocol::srv::Probe>("tank_calling", std::bind(&Tank::ServiceFunc, this, std::placeholders::_1, std::placeholders::_2));
   }
 
   void RunOnce() {
-    auto msg = protocol::msg::TankActive();
-    msg.activity = this->distance++;
-    printf("tank publish once: %d\n", msg.activity);
-    m_pPublisher->publish(msg);
+    while(true) {
+      auto msg = protocol::msg::TankActive();
+      msg.activity = this->distance++;
+      printf("tank publish once: %d\n", msg.activity);
+      m_pPublisher->publish(msg);
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
   }
 
 private:
   rclcpp::Publisher<protocol::msg::TankActive>::SharedPtr m_pPublisher;
+  rclcpp::Service<protocol::srv::Probe>::SharedPtr m_pService;
   uint8_t distance{0};
+
+  void ServiceFunc(const std::shared_ptr<tanksrv::Request> request, std::shared_ptr<tanksrv::Response> response) {
+    (void) request;
+    printf("tank service on call once\n");
+    response->distance = 1;
+    response->landform = 1;
+  }
+
 };
 
 void TestTankPublish() {
@@ -41,10 +58,8 @@ int main(int argc, char ** argv)
   rclcpp::init(argc, argv);
   std::shared_ptr<Tank> tank = std::make_shared<Tank>();
 
-  while(true) {
-    tank->RunOnce();
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
+  std::thread t = std::thread(TestTankPublish);
+  t.detach();
   rclcpp::spin(tank);
   rclcpp::shutdown();
 
